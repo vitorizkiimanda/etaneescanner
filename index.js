49ca597139a036d685ea4ec4dc345a4e9592177a
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-08-23 10:03:55 +0700

    new name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('etaneescanner', () => App);
+AppRegistry.registerComponent('yourganic3', () => App);
